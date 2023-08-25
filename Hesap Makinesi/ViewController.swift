53bfd1aa2f846b1256f719bbc2fc83b05674fea5
Fatih Oguz <oguzfatih39@gmail.com>
2023-08-25 18:39:15 +0300

    hesap makinesi tamamlandi

--- Hesap Makinesi/ViewController.swift
+++ Hesap Makinesi/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var textFieldOne: UITextField!
+    @IBOutlet weak var textFieldTwo: UITextField!
+    @IBOutlet weak var cevap: UILabel!
     override func viewDidLoad() {
+        
+        textFieldOne.keyboardType = UIKeyboardType.numberPad
+        
+        textFieldTwo.keyboardType = UIKeyboardType.numberPad
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func toplama(_ sender: Any) {
+        let result =  Int(textFieldOne.text!)! + Int( textFieldTwo.text!)!
+        
+        print(result)
+        cevap.text = String(result)
+        
+    }
+   
 
-
+    
+    
+    
+    
+    
+    @IBAction func cikarma(_ sender: Any) {
+        let result = Int(textFieldOne.text!)! - Int(textFieldTwo.text!)!
+        cevap.text = String(result)
+    }
+    
+    @IBAction func carpma(_ sender: Any) {
+        let result = Int(textFieldOne.text!)! * Int(textFieldTwo.text!)!
+        cevap.text = String(result)
+    }
+    
+    @IBAction func bolme(_ sender: Any) {
+        let result = Int(textFieldOne.text!)! / Int(textFieldTwo.text!)!
+        cevap.text = String(result)
+    }
+    
+    
+    
 }
+
+
 
